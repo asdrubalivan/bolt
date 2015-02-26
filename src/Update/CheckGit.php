9fc0c62326c40bc1a5081238e5dd056e18842207
Gawain Lynch <gawain.lynch@gmail.com>
2015-02-26 08:08:31 +0000

    Outline for git install update check

--- src/Update/CheckGit.php
+++ src/Update/CheckGit.php
@@ -1,1 +1,22 @@
 <?php
+
+namespace Bolt\Update;
+
+class CheckGit extends CheckAbstract implements CheckInterface
+{
+    /**
+     * {@inheritdoc }
+     */
+    public function getName()
+    {
+        return 'standard';
+    }
+
+    /**
+     * {@inheritdoc }
+     */
+    public function getType()
+    {
+        return 'standard';
+    }
+}
