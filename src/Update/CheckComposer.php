8c8d4bceb43f3e59b89e0176092ba59e0156313c
Gawain Lynch <gawain.lynch@gmail.com>
2015-02-26 08:08:42 +0000

    Outline for Composer install update check

--- src/Update/CheckComposer.php
+++ src/Update/CheckComposer.php
@@ -1,1 +1,22 @@
 <?php
+
+namespace Bolt\Update;
+
+class CheckComposer extends CheckAbstract implements CheckInterface
+{
+    /**
+     * {@inheritdoc }
+     */
+    public function getName()
+    {
+        return 'standard';
+    }
+
+    /**
+     * {@inheritdoc }
+     */
+    public function getType()
+    {
+        return 'standard';
+    }
+}
